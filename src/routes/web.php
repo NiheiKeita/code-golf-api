b160e1bb84fa45db6b61ad8521701fc0153414dd
NiheiKeita <86122925+NiheiKeita@users.noreply.github.com>
2024-04-18 23:22:45 +0900

    CODEの結果が正しいかのチェックAPIを作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CodeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/code-check', [CodeController::class, 'index'])->name('code_check');
