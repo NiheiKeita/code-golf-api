12cbb72846e2c5c8a262a47bca4c89614f0cda87
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-05-18 16:04:20 +0900

    いらないコードを削除

--- src/app/Http/Controllers/RankingController.php
+++ src/app/Http/Controllers/RankingController.php
@@ -10,11 +10,6 @@ class RankingController extends Controller
     public function index(Request $request)
     {
         $question = Question::find($request->id);
-        // $codes = $question->codes()->get()->groupBy('user_id')->map(function ($group) {
-        //     return $group->sortBy('code_byte')->first();
-        // })->sortBy('code_byte');
-        // $codes = $question->codeRanking();
-        // $codes = $question->codes()->maxCodeBytePerUser();
         $codes = $question->getMaxCodeBytePerUser();
         
         return response()->json(['codes' => $codes]);
