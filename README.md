713aea10274969f819c9a3dbd431df9ff3d1c953
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-07-06 19:41:24 +0900

    code golf第２回のための問題を作成

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # docker-laravel-app
  
+
+
+cat /etc/hosts | awk 'END{print $1}' | sed -e 's/[0-9]\+$/1/g'
--- src/app/Http/Controllers/CodeController.php
+++ src/app/Http/Controllers/CodeController.php
@@ -18,10 +18,10 @@ class CodeController extends Controller
         $output = ob_get_clean();
         return $output;
     }
-    function isCorrect($value): bool
+    function isCorrect($value, $answer): bool
     {
-        $correctValue ="1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34\nBuzz\nFizz\n37\n38\nFizz\nBuzz\n41\nFizz\n43\n44\nFizzBuzz\n46\n47\nFizz\n49\nBuzz\nFizz\n52\n53\nFizz\nBuzz\n56\nFizz\n58\n59\nFizzBuzz\n61\n62\nFizz\n64\nBuzz\nFizz\n67\n68\nFizz\nBuzz\n71\nFizz\n73\n74\nFizzBuzz\n76\n77\nFizz\n79\nBuzz\nFizz\n82\n83\nFizz\nBuzz\n86\nFizz\n88\n89\nFizzBuzz\n91\n92\nFizz\n94\nBuzz\nFizz\n97\n98\nFizz\nBuzz\n";
-        return $correctValue === $value;
+        // $correctValue ="1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34\nBuzz\nFizz\n37\n38\nFizz\nBuzz\n41\nFizz\n43\n44\nFizzBuzz\n46\n47\nFizz\n49\nBuzz\nFizz\n52\n53\nFizz\nBuzz\n56\nFizz\n58\n59\nFizzBuzz\n61\n62\nFizz\n64\nBuzz\nFizz\n67\n68\nFizz\nBuzz\n71\nFizz\n73\n74\nFizzBuzz\n76\n77\nFizz\n79\nBuzz\nFizz\n82\n83\nFizz\nBuzz\n86\nFizz\n88\n89\nFizzBuzz\n91\n92\nFizz\n94\nBuzz\nFizz\n97\n98\nFizz\nBuzz\n";
+        return $answer === $value;
     }
 
     #[OA\Post(
@@ -31,28 +31,37 @@ class CodeController extends Controller
             new OA\Response(response: 401, description: 'Not allowed'),
         ]
     )]
-    public function index(Request $request): Array
+    public function index(Request $request)
     {
+        $user = User::find($request->user_id);
+        $question = Question::find($request->question_id);
+        if(!$user) {
+            return response()->json([
+                'message' => 'user not found.'
+            ], 404);
+        }
+        if(!$question) {
+            return response()->json([
+                'message' => 'question not found.'
+            ], 404);
+        }
+    
         $codeCheck = new CodeCheck();
         $result = $codeCheck->codeCheck($request->code ?? "");
-        $isCorrect = self::isCorrect($result->response ?? "");
+        $isCorrect = self::isCorrect($result->response ?? "", $question->answer);
         $codeResult = !$result->error ? ($isCorrect ? "ok" : "ng") : "error";
         $byte = null;
-        $user = User::find($request->id);
-        $question = Question::find($request->question_id);
         if($codeResult === "ok"){
             $byte = strlen($request->code);
             self::createCode($request->code,$user,$question);
         }
-
-        $data = [
+        return response()->json([
             "result" => $codeResult,
             "response" => $result->response,
             "error" => $result->error,
             "code" => $request->code,
             "byte" => $byte,
-        ];
-        return $data;
+        ], 200);
     }
 
     public function check(Request $request): Array
@@ -65,7 +74,6 @@ class CodeController extends Controller
     }
 
     public function createCode($code, $user, $quesion){
-
         Code::create([
             "user_id" => $user->id,
             "question_id" => $quesion->id,
